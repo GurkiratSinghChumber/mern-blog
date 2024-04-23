ffc567118a8e56447dce113088fe996e23efab51
Gurkirat Singh Chumber <chumbgur@sheridancollege.ca>
2024-04-23 13:01:26 -0400

    Revert "Reapply "established database connection""
    This reverts commit 5e1a5f11663acc17c673510e1914824c4e45913f.

--- api/index.js
+++ api/index.js
@@ -1,25 +1,11 @@
 let express = require("express");
 var bodyParser = require("body-parser");
 require("dotenv").config();
-const mongoose = require("mongoose");
 
-const databaseUrl = process.env.DATABASEURL;
+let port = process.env.PORT;
 
-const port = process.env.PORT;
+let app = express();
 
-const app = express();
-
-const connectToDatabase = async (databaseUrl) => {
-  try {
-    await mongoose.connect(databaseUrl);
-    console.log("connected to database");
-    //listening to requests
-    app.listen(port, () => {
-      console.log(`listening at port ${port}`);
-    });
-  } catch (e) {
-    console.log(e);
-  }
-};
-
-connectToDatabase(databaseUrl);
+app.listen(port, () => {
+  console.log(`Listening at port number ${port}`);
+});
