5190bd6d275dea17c4fef50896564710a76a4136
Gurkirat Singh Chumber <chumbgur@sheridancollege.ca>
2024-04-25 19:32:04 -0400

    create sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 const User = require("../modles/user.model.js");
 const { errorHandler } = require("../utils/error.js");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -27,4 +28,36 @@ const signup = async (req, res, next) => {
   }
 };
 
-module.exports = { signup };
+const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  if (!email || !password || email === "" || password === "") {
+    next(errorHandler(400, "All feilds are required!"));
+  }
+
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(404, "User Not Found"));
+    }
+
+    const validPassword = bcrypt.compareSync(password, validUser.password);
+
+    if (!validPassword) {
+      return next(errorHandler(400, "Invalid Password"));
+    }
+
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: pass, ...rest } = validUser._doc;
+
+    res
+      .status(200)
+      .cookie("access-token", token, {
+        httpOnly: true,
+      })
+      .json(rest);
+  } catch (e) {
+    next(e);
+  }
+};
+
+module.exports = { signup, signin };
--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,10 +1,13 @@
 const express = require("express");
-const { signup } = require("../controllers/auth.controller.js");
+const { signup, signin } = require("../controllers/auth.controller.js");
 
 const router = express.Router();
 
 router.post("/sign-up", (req, res, next) => {
   signup(req, res, next);
+});
+router.post("/sign-in", (req, res, next) => {
+  signin(req, res, next);
 });
 
 module.exports = router;
